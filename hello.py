ed51704a809375eae5d8998c768676247cbfafa0
BernhardKonrad <bernhard.konrad@gmail.com>
2014-05-18 15:18:45 -0700

    First commit.

---
+++ hello.py
@@ -0,0 +1,26 @@
+from flask import Flask, request, render_template
+#from redis import Redis as redis
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+#    name = redis.get('name').decode('utf-8')
+    return render_template("communicate.html", name='abc')
+
+
+@app.route('/', methods=['POST'])
+def communicate_post():
+    if request.form['my-form'] == 'Send':
+        name = request.form['text']
+        redis.set('name',name)
+        return render_template("communicate.html", name=name)
+    elif request.form['my-form'] == 'Refresh':
+        name = redis.get('name').decode('utf-8')
+        return render_template("communicate.html", name=name)
+    else:
+        return "Something went wrong"
+
+if __name__ == "__main__":
+	app.run()
