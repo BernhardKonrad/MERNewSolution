707a8c10339d2251a56c2f923c33afe549aec635
BernhardKonrad <bernhard.konrad@gmail.com>
2014-05-23 23:41:28 -0700

    Adds connection to redis database

--- hello.py
+++ hello.py
@@ -1,13 +1,17 @@
 from flask import Flask, request, render_template
-#from redis import Redis as redis
+import redis
 
+redis = redis.StrictRedis(host = 'pub-redis-10073.us-east-1-3.3.ec2.garantiadata.com' , port = '10073' , db = 0)
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
 #    name = redis.get('name').decode('utf-8')
-    return render_template("communicate.html", myhint='', mysol='')
+    hint = redis.get('hint').decode('utf-8')
+    sol = redis.get('sol').decode('utf-8')
+    both = hint + sol
+    return render_template("communicate.html", myhint='', mysol='', both=both)
 
 
 @app.route('/', methods=['POST'])
@@ -15,9 +19,12 @@ def communicate_post():
     if request.form['submit'] == 'preview':
         hint = request.form['inputHint']
         sol = request.form['inputSol']
-        return render_template("communicate.html", myhint=hint, mysol=sol)
+        redis.set('hint', hint)
+        redis.set('sol', sol)
+        both = hint + sol
+        return render_template("communicate.html", myhint=hint, mysol=sol, both = both)
     else:
         return "Something went wrong"
 
 if __name__ == "__main__":
-	app.run()
+	app.run(debug=True)
