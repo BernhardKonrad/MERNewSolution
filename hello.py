a8c2bdaf39b059f79c52c3bf321c0cf207c3a4ba
BernhardKonrad <bernhard.konrad@gmail.com>
2014-06-14 17:54:27 -0700

    Displays hint, solution if loaded with the corresponding url

--- hello.py
+++ hello.py
@@ -2,12 +2,13 @@ from flask import Flask, request, render_template
 import redis
 import urllib
 import operator
+import pypandoc
+import re
 
 redis = redis.StrictRedis(
     host='pub-redis-10073.us-east-1-3.3.ec2.garantiadata.com',
     port='10073', db=0)
 app = Flask(__name__)
-
 
 class Tag():
     def __init__(self, name, onMER):
@@ -27,6 +28,30 @@ def read_tags():
         if not "<" in tag:
             list_tags.append(tag)
     return sorted(list_tags)
+
+def url_from_info(course, exam, question):
+    return ("http://wiki.ubc.ca/Science:Math_Exam_Resources/Courses/" + course + "/" + exam + "/" + question)
+
+def hint_from_question_url(base):
+    hint_url = base.replace('Science:', 'index.php?title=Science:') + '/Hint_1&action=raw'
+    hint_raw = urllib.urlopen(hint_url).read().decode('utf-8')
+    hint_raw = hint_raw.replace('\n', '').replace('<math>', '$').replace('</math>', '$').replace('$begin{align}', '\n $begin{align}').replace('end{align}$', 'end{align}$ \n\n')
+    hint = pypandoc.convert(hint_raw, 'latex', format='html')
+    return hint_raw
+
+def solution_from_question_url(base):
+    sol_url = base.replace('Science:', 'index.php?title=Science:') + '/Solution_1&action=raw'
+    sol_raw = urllib.urlopen(sol_url).read().decode('utf-8')
+    sol_raw = sol_raw.replace('\n', '').replace('<math>', '$').replace('</math>', '$').replace('$begin{align}', '\n $begin{align}').replace('end{align}$', 'end{align}$ \n\n')
+    sol = pypandoc.convert(sol_raw, 'latex', format='html')
+    return sol_raw
+
+@app.route('/<course>/<exam>/<question>')
+def show_existing(course, exam, question):
+    question_url = url_from_info(course, exam, question)
+    hint = hint_from_question_url(question_url)
+    solution = solution_from_question_url(question_url)
+    return render_template("communicate.html", myhint=hint, mysol=solution)
 
 @app.route('/')
 def hello():
